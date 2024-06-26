123b523b684238ac05dbeb88eee8ba250bbd8eba
KobengChantel <kobengchantel@gmail.com>
2024-06-26 09:17:24 +0200

    done

---
+++ bookstore_mern/backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+// export const mongoDBURL ='mongodb+srv://kobengchantel:kobengchanty@firstwork@bookstoremern.af61tij.mongodb.net/?retryWrites=true&w=majority&appName=bookstoreMern'
+export const mongoDBURL =
+'mongodb+srv://kobengchantel:kobengchanty@bookstoremern.af61tij.mongodb.net/?retryWrites=true&w=majority&appName=bookstoreMern'
+
+// export const mongoDBURL =
+// 'mongodb+srv://kobengchantel:kobengchanty@firstwork@bookstoremern.af61tij.mongodb.net/'
