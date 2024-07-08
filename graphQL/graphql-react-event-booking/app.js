7587b0bba2099a6225bb59da0b1fa89aabefc16d
KobengChantel <kobengchantel@gmail.com>
2024-07-08 10:50:12 +0200

    push

--- graphQL/graphql-react-event-booking/app.js
+++ graphQL/graphql-react-event-booking/app.js
@@ -2,11 +2,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema} = require('graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); 
+
 
 const app = express();
+ const Event = require('./models/event');
+const User = require('./models/user');
 
-//parse incoming json bodies
 app.use(bodyParser.json());
 
 app.use(
@@ -19,21 +23,35 @@ app.use(
           description: String!
           price: Float!
           date: String!
+          creator: User!
         }
 
-         input EventInput {
+          type User {
+          _id: ID!
+          email: String!
+          password: String
+          createdEvents: [Event!]
+        }
+
+        input EventInput {
           title: String!
           description: String!
           price: Float!
           date: String!
         }
 
+        input UserInput {
+          email: String!
+          password: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+             createUser(userInput: UserInput): User
         }
 
         schema {
@@ -43,31 +61,99 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
-      createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
-          title: args.title,
-          description: args.description,
-          price: +args.price,
-          date: args.date
-        }
-        console.log(event);
-        events.push(event);
-        return event;
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+          // creator: '5c0f6dcde049d205fa2471dc'
+          creator: '6687e118b7b420bd01923406'
+        });
+        let createdEvent;
+        return event
+          .save()
+          .then(result => {
+            createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+            return User.findById('6687e118b7b420bd01923406');
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error('User not found.');
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error('User exists already.');
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch(err => {
+            throw err;
+          });
       }
     },
     graphiql: true
   })
 );
 
+// mongoose.connect(
+//   `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ryimgla.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`
+// ).then(() => {
+//    app.listen(3000);
+//  })
+//  .catch(err => {
+//    console.log(err);
+//  });
+
+
+ mongoose.connect('mongodb+srv://graphql:12345@cluster0.ryimgla.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0')
+
+.then(() => {
+  console.log('Successfully connected to MongoDB');
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+})
+.catch(err => {
+  console.error('Connection error:', err);
+});
+
 app.get('/', (req, res, next) =>{
   res.send('Hello world');
-})
-
-
-app.listen(3000);
+});
 
 
 
@@ -87,33 +173,3 @@ app.listen(3000);
 
 
 
-// # Welcome to GraphiQL
-// #
-// # GraphiQL is an in-browser tool for writing, validating, and
-// # testing GraphQL queries.
-// #
-// # Type queries into this side of the screen, and you will see intelligent
-// # typeaheads aware of the current GraphQL type schema and live syntax and
-// # validation errors highlighted within the text.
-// #
-// # GraphQL queries typically start with a "{" character. Lines that start
-// # with a # are ignored.
-// #
-// # An example GraphQL query might look like:
-
-// #     {
-// #       field(arg: "value") {
-// #         subField
-// #       }
-// #     }
-// #
-// # Keyboard shortcuts:
-// #
-// #  Prettify Query:  Shift-Ctrl-P (or press the prettify button above)
-// #
-// #     Merge Query:  Shift-Ctrl-M (or press the merge button above)
-// #
-// #       Run Query:  Ctrl-Enter (or press the play button above)
-// #
-// #   Auto Complete:  Ctrl-Space (or just start typing)
-// #
