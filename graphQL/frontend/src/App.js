5a20bf7d9cc94c53aa01e0a8e6067e78ae43560f
KobengChantel <kobengchantel@gmail.com>
2024-07-12 13:00:32 +0200

    doing front end

--- graphQL/frontend/src/App.js
+++ graphQL/frontend/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Navigate to="/auth" />} />
+              <Route path="/auth" element={<AuthPage />} />
+              <Route path="/events" element={<EventsPage />} />
+              <Route path="/bookings" element={<BookingsPage />} />
+            </Routes>
+          </main>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
