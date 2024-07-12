239a70786fe9c60ae6cf889c63db59d9cdd88f50
KobengChantel <kobengchantel@gmail.com>
2024-07-12 14:28:11 +0200

    finishing up

--- graphQL/frontend/src/App.js
+++ graphQL/frontend/src/App.js
@@ -5,23 +5,59 @@ import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
 import MainNavigation from './components/Navigation/MainNavigation';
+import AuthContext from './context/auth-context';
+
 
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    token: null,
+    userId: null
+  };
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token: token, userId: userId });
+  };
+
+  logout = () => {
+    this.setState({ token: null, userId: null });
+  };
+
+
   render() {
     return (
       <BrowserRouter>
         <React.Fragment>
+
+        <AuthContext.Provider
+            value={{
+              token: this.state.token,
+              userId: this.state.userId,
+              login: this.login,
+              logout: this.logout
+            }}
+          >
+
           <MainNavigation />
           <main className="main-content">
             <Routes>
-              <Route path="/" element={<Navigate to="/auth" />} />
-              <Route path="/auth" element={<AuthPage />} />
-              <Route path="/events" element={<EventsPage />} />
-              <Route path="/bookings" element={<BookingsPage />} />
-            </Routes>
+            {!this.state.token && <Route path="/" element={<Navigate to="/auth" replace />} />}
+                {this.state.token && <Route path="/" element={<Navigate to="/events" replace />} />}
+                {this.state.token && <Route path="/auth" element={<Navigate to="/events" replace />} />}
+                {!this.state.token && (
+                  <Route path="/auth" element={<AuthPage />} />
+                )}
+                <Route path="/events" element={<EventsPage />} />
+                {this.state.token && (
+                  <Route path="/bookings" element={<BookingsPage />} />
+                )}
+                {!this.state.token && <Route path="*" element={<Navigate to="/auth" replace />} />}
+              </Routes>
+
           </main>
+          </AuthContext.Provider>
         </React.Fragment>
       </BrowserRouter>
     );
--- graphQL/mern-graphql-react-event-booking/app.js
+++ graphQL/mern-graphql-react-event-booking/app.js
@@ -6,10 +6,25 @@ const mongoose = require('mongoose');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use(isAuth);
+
 
 app.use(
   '/graphql',
@@ -36,8 +51,8 @@ app.use(
 
 .then(() => {
   console.log('Successfully connected to MongoDB');
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  app.listen(8000, () => {
+    console.log('Server is running on port 8000');
   });
 })
 .catch(err => {
