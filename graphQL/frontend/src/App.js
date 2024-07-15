5eba1357d4e02d6c6f4f87f2ffe834e0b1af0ef8
KobengChantel <kobengchantel@gmail.com>
2024-07-15 12:57:00 +0200

    done

--- graphQL/frontend/src/App.js
+++ graphQL/frontend/src/App.js
@@ -7,11 +7,9 @@ import EventsPage from './pages/Events';
 import MainNavigation from './components/Navigation/MainNavigation';
 import AuthContext from './context/auth-context';
 
-
 import './App.css';
 
 class App extends Component {
-
   state = {
     token: null,
     userId: null
@@ -25,13 +23,11 @@ class App extends Component {
     this.setState({ token: null, userId: null });
   };
 
-
   render() {
     return (
       <BrowserRouter>
         <React.Fragment>
-
-        <AuthContext.Provider
+          <AuthContext.Provider
             value={{
               token: this.state.token,
               userId: this.state.userId,
@@ -39,24 +35,17 @@ class App extends Component {
               logout: this.logout
             }}
           >
-
-          <MainNavigation />
-          <main className="main-content">
-            <Routes>
-            {!this.state.token && <Route path="/" element={<Navigate to="/auth" replace />} />}
-                {this.state.token && <Route path="/" element={<Navigate to="/events" replace />} />}
+            <MainNavigation />
+            <main className="main-content">
+              <Routes>
+                <Route path="/" element={<Navigate to={this.state.token ? "/events" : "/auth"} replace />} />
                 {this.state.token && <Route path="/auth" element={<Navigate to="/events" replace />} />}
-                {!this.state.token && (
-                  <Route path="/auth" element={<AuthPage />} />
-                )}
+                {!this.state.token && <Route path="/auth" element={<AuthPage />} />}
                 <Route path="/events" element={<EventsPage />} />
-                {this.state.token && (
-                  <Route path="/bookings" element={<BookingsPage />} />
-                )}
+                {this.state.token && <Route path="/bookings" element={<BookingsPage />} />}
                 {!this.state.token && <Route path="*" element={<Navigate to="/auth" replace />} />}
               </Routes>
-
-          </main>
+            </main>
           </AuthContext.Provider>
         </React.Fragment>
       </BrowserRouter>
