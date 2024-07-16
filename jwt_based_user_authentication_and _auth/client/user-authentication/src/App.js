23a56b72a5e051e28c01c096d9fa50e2d3d26054
KobengChantel <kobengchantel@gmail.com>
2024-07-16 15:10:28 +0200

    done

--- jwt_based_user_authentication_and _auth/client/user-authentication/src/App.js
+++ jwt_based_user_authentication_and _auth/client/user-authentication/src/App.js
@@ -1,13 +1,18 @@
 
 
 
+import { Route, Routes } from "react-router-dom";
+import { Login, Signup } from "./pages";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-      
-    
-          <h1>Hello, Learn React</h1>
-       
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
--- jwt_based_user_authentication_and _auth/server/Routes/AuthRoute.js
+++ jwt_based_user_authentication_and _auth/server/Routes/AuthRoute.js
@@ -1,6 +1,11 @@
-const { Signup } = require("../Controllers/AuthController");
+
+
+const { Signup, Login} = require("../Controllers/AuthController");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
-
+router.post("/login", Login);
+// router.post("/login", Login);
 module.exports = router;
+
+
