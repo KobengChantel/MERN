1de2378f99625f27fcfc96b02bb8544378691647
KobengChantel <kobengchantel@gmail.com>
2024-08-27 15:10:06 +0200

    push

--- book_store/backend/config.js
+++ book_store/backend/config.js
@@ -4,5 +4,3 @@ export const mongoDBURL =
   // "mongodb+srv://book:book@cluster0.25gvldv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
   'mongodb+srv://book:book@cluster0.zcgn0eu.mongodb.net/book-collections?retryWrites=true&w=majority&appName=Cluster0'
 
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
